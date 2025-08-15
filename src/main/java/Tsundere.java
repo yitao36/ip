8975075a4ac2d608ad54971aa6121717d0392043
yitao36 <yitao36g@gmail.com>
2025-08-15 15:54:02 +0800

    Add Echo commands

--- src/main/java/Tsundere.java
+++ src/main/java/Tsundere.java
@@ -1,3 +1,7 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
 public class Tsundere {
     public static final int WIDTH = 40;
     public static final String HORIZONTAL_LINE = "_".repeat(WIDTH);
@@ -11,12 +15,15 @@ public class Tsundere {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        System.out.println("What do you want...? I'm obviously busy!");
+
+        System.out.println(HORIZONTAL_LINE);
     }
 
     public static void exit() {
         System.out.println(HORIZONTAL_LINE);
 
-        String exitMessage = "See you!";
+        String exitMessage = "Hmph, I won't miss you!";
         System.out.println(exitMessage);
 
         System.out.println(HORIZONTAL_LINE);
@@ -24,6 +31,25 @@ public class Tsundere {
 
     public static void main(String[] args) {
         init();
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Scanner sc = new Scanner(br);
+
+        outer:
+        while (true) {
+            String line = sc.nextLine();
+            String[] words = line.split(" ");
+
+            String command = words.length > 0 ? words[0] : "noInput";
+
+            switch (line) {
+                case "bye":
+                    break outer;
+                default:
+                    System.out.println(line);
+                    continue;
+            }
+        }
 
         exit();
     }
