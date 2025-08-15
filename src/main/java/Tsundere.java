f1d0da8d4afba7985a91461270fe6f33eed0c9cb
yitao36 <yitao36g@gmail.com>
2025-08-15 15:46:08 +0800

    Rename to Tsundere

---
+++ src/main/java/Tsundere.java
@@ -0,0 +1,30 @@
+public class Tsundere {
+    public static final int WIDTH = 40;
+    public static final String HORIZONTAL_LINE = "_".repeat(WIDTH);
+
+    public static void init() {
+        System.out.println(HORIZONTAL_LINE);
+
+        String logo = " ____        _        \n"
+                + "|  _ \\ _   _| | _____ \n"
+                + "| | | | | | | |/ / _ \\\n"
+                + "| |_| | |_| |   <  __/\n"
+                + "|____/ \\__,_|_|\\_\\___|\n";
+        System.out.println("Hello from\n" + logo);
+    }
+
+    public static void exit() {
+        System.out.println(HORIZONTAL_LINE);
+
+        String exitMessage = "See you!";
+        System.out.println(exitMessage);
+
+        System.out.println(HORIZONTAL_LINE);
+    }
+
+    public static void main(String[] args) {
+        init();
+
+        exit();
+    }
+}
