2a1aa9e8c59afec237151d7b9fdbed9136a271ae
yitao36 <yitao36g@gmail.com>
2025-08-15 17:29:25 +0800

    Add Task class, mark and undone commands

--- src/main/java/Tsundere.java
+++ src/main/java/Tsundere.java
@@ -1,24 +1,45 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.Arrays;
+import java.util.Optional;
 import java.util.Scanner;
+
+class Task {
+    private final String name;
+    private boolean done = false;
+
+    public Task(String name) {
+        this.name = name;
+    }
+
+    public boolean isDone() {
+        return done;
+    }
+
+    public void markDone() {
+        this.done = true;
+    }
+
+    public void markUndone() {
+        this.done = false;
+    }
+
+    public String toString() {
+        return "[" + (done ? "X" : "") + "] " + this.name;
+    }
+}
 
 public class Tsundere {
     public static final int WIDTH = 40;
     public static final String HORIZONTAL_LINE = "_".repeat(WIDTH);
-    public static final String[] tasks = new String[100];
+    public static final Task[] tasks = new Task[100];
     public static int numTasks = 0;
 
     public static void init() {
         System.out.println(HORIZONTAL_LINE);
 
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
-        System.out.println("What do you want...? I'm obviously busy!");
+        System.out.println("Oh! I-I- didn't expect you to be here...");
+        System.out.println("What do you want...? Hmph, I'm obviously busy!");
 
         System.out.println(HORIZONTAL_LINE);
     }
@@ -26,7 +47,7 @@ public class Tsundere {
     public static void exit() {
         System.out.println(HORIZONTAL_LINE);
 
-        String exitMessage = "Hmph, I won't miss you!";
+        String exitMessage = "Oh... Leaving already? N-not that I care! Bye...";
         System.out.println(exitMessage);
 
         System.out.println(HORIZONTAL_LINE);
@@ -35,9 +56,9 @@ public class Tsundere {
     public static void addTodo(String message) {
         System.out.println(HORIZONTAL_LINE);
 
-        tasks[numTasks] = message;
+        tasks[numTasks] = new Task(message);
         numTasks++;
-        System.out.println("I've added " + message + " to the task list.");
+        System.out.println("I've added " + message + " to the list.");
 
         System.out.println(HORIZONTAL_LINE);
     }
@@ -52,6 +73,54 @@ public class Tsundere {
         for (int i = 0; i < numTasks; i++) {
             System.out.print((i+1) + ". ");
             System.out.println(tasks[i]);
+        }
+
+        System.out.println(HORIZONTAL_LINE);
+    }
+
+    public static void mark(String idString) {
+        System.out.println(HORIZONTAL_LINE);
+
+        int id;
+        try {
+            id = Integer.parseInt(idString) - 1;
+            if (id >= numTasks || id < 0) {
+                throw new ArrayIndexOutOfBoundsException();
+            } else if (tasks[id].isDone()) {
+                System.out.println("Ehh? It's already marked! You probably input the wrong number, silly.");
+                System.out.println(tasks[id]);
+            }
+            tasks[id].markDone();
+            System.out.println("Here, it's marked.");
+            System.out.println(tasks[id]);
+
+        } catch (NumberFormatException e) {
+            System.out.println("That's not a valid number, baka!");
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Ugh! That task does not exist. You know I'm busy, right?");
+        }
+
+        System.out.println(HORIZONTAL_LINE);
+    }
+
+    public static void unmark(String idString) {
+        int id;
+        try {
+            id = Integer.parseInt(idString) - 1;
+            if (id >= numTasks || id < 0) {
+                throw new ArrayIndexOutOfBoundsException();
+            } else if (!tasks[id].isDone()) {
+                System.out.println("Dude!! That task isn't even done yet!");
+                System.out.println(tasks[id]);
+            }
+            tasks[id].markUndone();
+            System.out.println("Why'd you even mark it done?");
+            System.out.println(tasks[id]);
+
+        } catch (NumberFormatException e) {
+            System.out.println("That's not a valid number, baka!");
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Ugh! That task does not exist. You know I'm busy, right?");
         }
 
         System.out.println(HORIZONTAL_LINE);
@@ -84,6 +153,18 @@ public class Tsundere {
                     break;
                 case "list":
                     list();
+                    break;
+                case "mark":
+                    String id = words.length > 1
+                            ? words[1]
+                            : null;
+                    mark(id);
+                    break;
+                case "unmark":
+                    String id2 = words.length > 1
+                            ? words[1]
+                            : null;
+                    unmark(id2);
                     break;
                 case "bye":
                     break outer;
