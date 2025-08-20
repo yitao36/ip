02d5e29f6b3f3aa374fc2c14aaf0ae5b603d0d7b
yitao36 <yitao36g@gmail.com>
2025-08-20 23:23:19 +0800

    InvalidFormatCommand: use enums in constructor instead of String
    Enums is more specific than Strings, and enables auto-complete for programmers,
    thus clearer for readers.
    
    Let's,
    * use enums to denote the possible cases to handle

--- src/main/java/tsundere/command/InvalidFormatCommand.java
+++ src/main/java/tsundere/command/InvalidFormatCommand.java
@@ -5,8 +5,13 @@ import tsundere.task.TaskList;
 import tsundere.ui.Ui;
 
 public class InvalidFormatCommand extends AbstractCommand {
-    private final String format;
-    public InvalidFormatCommand(String format) {
+    public enum Format {
+            TODO, DEADLINE, EVENT, EVENT_DATE, MARK, UNMARK, DELETE, HELP
+    }
+
+    private final Format format;
+
+    public InvalidFormatCommand(Format format) {
         super(false);
         this.format = format;
     }
