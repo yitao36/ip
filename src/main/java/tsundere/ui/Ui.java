fe481acd145b415a78cdfdb5ec2610d15c61df91
yitao36 <yitao36g@gmail.com>
2025-08-24 17:28:15 +0800

    Reformat according to coding standard

--- src/main/java/tsundere/ui/Ui.java
+++ src/main/java/tsundere/ui/Ui.java
@@ -1,7 +1,9 @@
 package tsundere.ui;
 
 import tsundere.command.InvalidFormatCommand;
+
 import static tsundere.command.InvalidFormatCommand.Format;
+
 import tsundere.task.Task;
 import tsundere.task.TaskList;
 
@@ -105,46 +107,46 @@ public class Ui {
         System.out.println(HORIZONTAL_LINE);
 
         switch (format) {
-            case HELP -> {
-                System.out.println("Here's a list of commands for your pea-brain.\n");
-                System.out.println("todo - Creates a todo task");
-                System.out.println("event - Creates an event task");
-                System.out.println("deadline - Creates a deadline task");
-                System.out.println("list - Lists all your tasks");
-                System.out.println("mark - Marks a task as completed");
-                System.out.println("unmark - Marks a task as uncompleted");
-                System.out.println("bye - Installs a virus! Don't input this");
-            }
-            case TODO -> {
-                System.out.println("Wrong format!");
-                System.out.println("Use `todo <name>`");
-            }
-            case DEADLINE -> {
-                System.out.println("Wrong format!");
-                System.out.println("Use `deadline <name> /by <YYYY-MM-DDTHH:MM>`");
-                System.out.println("Example: deadline TASK /by 2025-02-28T23:59");
-            }
-            case EVENT -> {
-                System.out.println("Wrong format!");
-                System.out.println("Use `event <name> /from <YYYY-MM-DDTHH:MM> \n/to <YYYY-MM-DDTHH:MM>`");
-                System.out.println("Example: `event TASK /from 2025-02-28T23:59 \n/to 2025-02-29T23:59`");
-            }
-            case EVENT_DATE -> {
-                System.out.println("Invalid date order!");
-                System.out.println("From date should be before To date!");
-            }
-            case MARK -> {
-                System.out.println("Wrong format!");
-                System.out.println("Use `mark <position of task in list>`");
-            }
-            case UNMARK -> {
-                System.out.println("Wrong format!");
-                System.out.println("Use `unmark <position of task in list>`");
-            }
-            case DELETE -> {
-                System.out.println("Wrong format!");
-                System.out.println("Use `delete <position of task in list>`");
-            }
+        case HELP -> {
+            System.out.println("Here's a list of commands for your pea-brain.\n");
+            System.out.println("todo - Creates a todo task");
+            System.out.println("event - Creates an event task");
+            System.out.println("deadline - Creates a deadline task");
+            System.out.println("list - Lists all your tasks");
+            System.out.println("mark - Marks a task as completed");
+            System.out.println("unmark - Marks a task as uncompleted");
+            System.out.println("bye - Installs a virus! Don't input this");
+        }
+        case TODO -> {
+            System.out.println("Wrong format!");
+            System.out.println("Use `todo <name>`");
+        }
+        case DEADLINE -> {
+            System.out.println("Wrong format!");
+            System.out.println("Use `deadline <name> /by <YYYY-MM-DDTHH:MM>`");
+            System.out.println("Example: deadline TASK /by 2025-02-28T23:59");
+        }
+        case EVENT -> {
+            System.out.println("Wrong format!");
+            System.out.println("Use `event <name> /from <YYYY-MM-DDTHH:MM> \n/to <YYYY-MM-DDTHH:MM>`");
+            System.out.println("Example: `event TASK /from 2025-02-28T23:59 \n/to 2025-02-29T23:59`");
+        }
+        case EVENT_DATE -> {
+            System.out.println("Invalid date order!");
+            System.out.println("From date should be before To date!");
+        }
+        case MARK -> {
+            System.out.println("Wrong format!");
+            System.out.println("Use `mark <position of task in list>`");
+        }
+        case UNMARK -> {
+            System.out.println("Wrong format!");
+            System.out.println("Use `unmark <position of task in list>`");
+        }
+        case DELETE -> {
+            System.out.println("Wrong format!");
+            System.out.println("Use `delete <position of task in list>`");
+        }
         }
 
         System.out.println(HORIZONTAL_LINE);
